394c4bacf5f75b24480c0bb7b4c0a1dccb637fb2
Yuri Kuznetsov <yuri.kuznetsov@revolutioninsure.com>
2018-07-24 18:28:47 +0300

    add new html in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `
+      <div><h1>{{pageTitle}}</h1>
+         <div>My first component</div>
+      </div>
+  `,
+
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  pageTitle: string = `Acme Product Mangement`;
   title = 'Angular: Getting Started';
 }
