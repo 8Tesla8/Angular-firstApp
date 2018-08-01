7b79e538fbd2417e68c8c8e6b49f5c8d9c49e951
Yuri Kuznetsov <yuri.kuznetsov@revolutioninsure.com>
2018-08-01 14:49:54 +0300

    add css file

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
+    styleUrls: [ './product-list.component.css' ]
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-
+    
     imageWidth: number = 50;
     imageMargin: number = 2;
-
+    
     showImage: boolean = false;
-
+    
     listFilter: string = "cart";
-
+    
     products: IProduct[] = [
         {
             "productId": 1,
@@ -38,7 +39,10 @@ export class ProductListComponent {
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
     ];
-
+        
+    ngOnInit(): void {
+        console.log("in ngOnInit");
+    }
     
     toggleImage() : void {
         this.showImage = !this.showImage;
